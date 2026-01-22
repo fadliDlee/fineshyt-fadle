442417a00aae42694de9257d021d472031d74c1f
fadliDlee <fadlicrzn123@gmail.com>
2026-01-23 02:22:44 +0700

    Ganti total config ke mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Abaikan error TypeScript (biar lolos deploy)
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // Abaikan error ESLint (biar lolos deploy)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
