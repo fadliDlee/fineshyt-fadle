e639bb8d41b60a29f68bbdd9fe8df13a24a18f95
fadliDlee <fadlicrzn123@gmail.com>
2026-01-23 02:04:59 +0700

    Fix tipe data gallery

--- src/types/idol.ts
+++ src/types/idol.ts
@@ -1,16 +1,18 @@
 export interface Idol {
-    id: number;
-    name: string;
-    group_name: string;
-    image_url: string;
-    zodiac?: string;
-    full_name?: string;
-    birth_date?: string;
-    birth_place?: string;
-    nationality?: string;
-    height?: string;
-    blood_type?: string;
-    mbti?: string;
-    description?: string;
-    likes?: number;
-  }
+  id: number;
+  created_at: string;
+  name: string;
+  group_name: string;
+  image_url: string;
+  full_name?: string;
+  birth_date?: string;
+  birth_place?: string;
+  nationality?: string;
+  height?: string;
+  blood_type?: string;
+  mbti?: string;
+  zodiac?: string;
+  description?: string;
+  gallery?: string[]; // 👈 PASTIKAN INI ADA!
+  likes?: number;     // 👈 PASTIKAN INI JUGA ADA (buat fitur love)
+}
